b073cd98596c92a28f651c4d732a4b0f184f5e00
Tiago-MM <machado.monteiro.tiago@gmail.com>
2026-01-08 14:07:38 +0100

    update new models

--- models/audio_models.py
+++ models/audio_models.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
 import tf_keras as keras
+import os
+
+# 2 dossiers au-dessus du fichier actuel
+script_dir = os.path.dirname(__file__)
 
 def load_unified_model(model_name):
     # Les architectures pour Repo 1 (Audio) et Repo 2 (Image)
     if "VGG16" in model_name:
-        return keras.models.load_model('models/my_vgg16.h5')
+        return keras.models.load_model(os.path.join(script_dir, '..', '..', 'models', 'my_vgg16.h5'))
     elif "ResNet" in model_name:
-        return keras.models.load_model('models/my_resnet.h5')
+        return keras.models.load_model(os.path.join(script_dir, '..', '..', 'models', 'my_resnet.h5'))
     elif "AlexNet" in model_name:
-        return keras.applications.resnet50.ResNet50(weights='imagenet')
+        return keras.models.load_model(os.path.join(script_dir, '..', '..', 'models', 'my_alexnet.h5'))
     elif "MobileNet" in model_name:
-        return keras.models.load_model('models/my_mobilenet.h5')
+        return keras.models.load_model(os.path.join(script_dir, '..', '..', 'models', 'my_mobilenet.h5'))
     elif "DenseNet" in model_name:
-        return keras.applications.densenet.DenseNet121(weights='imagenet')
+        return keras.models.load_model(os.path.join(script_dir, '..', '..', 'models', 'my_densenet.h5'))
+    
     return keras.applications.vgg16.VGG16(weights='imagenet')
